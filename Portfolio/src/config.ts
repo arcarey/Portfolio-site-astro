4080794b981919e40161123f11dbd979ef6ed80f
Andrew Carey <andrew.r.carey@gmail.com>
2023-01-22 19:57:22 -0600

    basic layouts finished

--- Portfolio/src/config.ts
+++ Portfolio/src/config.ts
@@ -1,5 +1,17 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
+export const SITE_TITLE = 'Andrew Carey';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const ColorTheme = {
+    color : {
+        colorText: '#1F2F35',
+        colorOne: '#C8CAC9',
+        colorBackground: '#8D9DA2',
+        colorThree: '#968F8C',
+        color: '#f3f3f3'
+
+
+    }
+}
